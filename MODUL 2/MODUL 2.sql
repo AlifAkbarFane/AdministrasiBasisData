11bb9ff2a3e8a77ca78d4f04b38e9bd6eb9152c2
AlifAkbarFane <alifa27kbar@gmail.com>
2026-07-07 06:16:11 +0800

    Update MODUL 2.sql

--- MODUL 2/MODUL 2.sql
+++ MODUL 2/MODUL 2.sql
@@ -1,50 +1,61 @@
--- Nama	: Alif Akbar Fane
--- Kelas: PTIB(4)
--- NIM	: 24241072
--- Modul: 2
+-- nama		: alif akbar fane
+-- kelas	: PTI.B / 4
+-- NIM		: 24241072
+-- Modul	: 2
 
--- gunakan databse
+
 USE db_mart;
 
--- cek deskripsi tabel
-describe pelanggan; 
-
--- statment SELECT 
--- mengambil satu kolom dalam tabel
+-- SELECT statement
+-- praktek 1
+-- ambil kolom nama produk dari tabel produk
 SELECT nama_produk FROM produk;
 
--- mengambil data lebih dari satu kolom
+-- praktek 2
+-- ambil kolom nama produk dan harga dari tabel produk
 SELECT nama_produk, harga FROM produk;
 
--- mengambil semua kolom pada tabel
-SELECT * FROM produk;
+-- praktek 3
+-- ambil seluruh kolom menggunakan * 
+SELECT * FROM ms_produk;
 
+-- praktek 4
+-- ambil kolom kode_produk, nama_produk dari tabel produk
 SELECT kode_produk, nama_produk FROM produk;
+
+-- ambil seluruh kolom dari tabel penjualan
 SELECT * FROM penjualan;
 
 -- PREFIX dan ALIAS
--- PREFIX, adalah bagian dari objek database yang jirarki lebih tinggi
--- nama_produk = column,
--- produk = tabel
--- db_mart = database 
-SELECT  produk. nama_produk FROM produk;
--- kolom ke tabel ke database 
+-- prefix
+-- praktek 5
+-- ambil nama produk dari tabel produk dan gunakan tabel sebagai prefix sebagai identitas bahwa nama produk punya produk
+SELECT produk.nama_produk  FROM produk;
+
+-- praktek 6
+-- ambil nama produk dari tabel produk, gunakan tabel dan database sebagai prefix
 SELECT db_mart.produk.nama_produk FROM produk;
 
--- Alias, nama sementara dari objek database
--- menggunakan kata kunci 'as'
--- alias pada objek (column)
-SELECT nama_produk as np FROM produk;
+-- alias = nama sementara pengganti nama objek database
+-- praktek 7
+-- ambil kolom nama produk dari tabel produk asliaskan nama produk dengan np
+SELECT nama_produk AS barang FROM produk;
+SELECT nama_produk np FROM produk;
 
--- alias pada objek tabel
-SELECT nama_produk FROM produk as pr;
+-- praktek 8
+-- alias tanpa keywoard as
+SELECT p.nama_produk, p.harga FROM produk p;
 
--- penggunaan prefix dan alias dalam satu querry
-SELECT pr.nama_produk FROM produk as pr;
+-- praktek 9
+-- menggunakan alias dan prefix bersamaan
+-- ambil nama produk dari tabel produk, aliaskan tabel produk dengan msp dan jadikan prefix untuk kolom
+SELECT msp.nama_produk FROM produk AS msp;
 
--- LATIHAN MANDIRI 
 -- case 1
-SELECT pr.nama_pelanggan, pr.alamat FROM pelanggan as pr;
+-- ambil nama pelanggan dan alamat dari tabel pelanggan
+SELECT nama_pelanggan, alamat FROM pelanggan;
 
 -- case 2
-SELECT pr.nama_produk, pr.harga FROM produk as pr;
+-- ambil nama produk dan harga dari tabael produk
+SELECT nama_produk, harga FROM produk;
+
